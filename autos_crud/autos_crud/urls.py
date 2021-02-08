8dc767b79a473433b56f73cb2ae68efd92753f0d
Marcio Woitek <woitek@usp.br>
2021-02-08 12:35:05 -0300

    Solution to 2nd assignment

---
+++ autos_crud/autos_crud/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/main.html')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('autos/', include('autos.urls')),
+    path('hello/', include('hello.urls')),
+    path('polls/', include('polls.urls')),
+]
